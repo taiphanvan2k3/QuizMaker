a92fd2722bc09d6a54cf36ab86d65a3f9ad7872a
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-04-25 08:24:33 +0700

    feat: [Quiz] Create functions for getting blueprint and env

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,18 +1,8 @@
-from flask import Blueprint
-from jinja2 import FileSystemLoader, Environment
-import os
-
-auth_bp = Blueprint("auth", __name__, template_folder="templates")
+from ..utils import helpers
+auth_bp = helpers.create_blueprint("auth", __name__)
 
 # Tạo biến env để load template từ các thư mục khác nhau
-env = Environment(
-    loader=FileSystemLoader(
-        [
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "auth", "templates"),
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "shared", "templates"),
-        ]
-    )
-)
+env = helpers.create_environment(__file__)
 
-# Import các route từ views.py
+# Import các route từ controller
 from . import controller
--- app/home/__init__.py
+++ app/home/__init__.py
@@ -1,19 +1,8 @@
-from flask import Blueprint
-from jinja2 import FileSystemLoader, Environment
-import os
-
-
-home_bp = Blueprint("home", __name__, template_folder="templates")
+from ..utils import helpers
+home_bp = helpers.create_blueprint("home", __name__)
 
 # Tạo biến env để load template từ các thư mục khác nhau
-env = Environment(
-    loader=FileSystemLoader(
-        [
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "home", "templates"),
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "shared", "templates"),
-        ]
-    )
-)
+env = helpers.create_environment(__file__)
 
-# Import các route từ views.py
+# Import các route từ controller
 from . import controller
