a5e4d5c2a200d4a8fc02e7cee4ec6f735ac2315f
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-04-28 00:25:32 +0700

    feat: [Quiz] Create the header

--- app/blueprints/section_class/controller.py
+++ app/blueprints/section_class/controller.py
@@ -5,5 +5,5 @@ from ..utils.helpers import render_template_util
 @section_class_bp.route("/")
 def index():
     return render_template_util(
-        env, "index.html", title="Học phần của bạn"
+        env, "index.html", title="Học phần của bạn", active_menu="section_class"
     )
