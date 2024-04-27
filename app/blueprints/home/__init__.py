2480f47084b5a0e58b8e3461b5870bb4ad2f46a2
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-04-27 16:23:51 +0700

    feat: [Quiz] Rebuild the folder structure and use static files

--- app/blueprints/home/__init__.py
+++ app/blueprints/home/__init__.py
@@ -1,8 +1,12 @@
+from flask import url_for
 from ..utils import helpers
-auth_bp = helpers.create_blueprint("auth", __name__)
+home_bp = helpers.create_blueprint("home", __name__)
 
 # Tạo biến env để load template từ các thư mục khác nhau
 env = helpers.create_environment(__file__)
+
+# Đăng ký hàm url_for vào global để có thể sử dụng trong template
+env.globals["url_for"] = url_for
 
 # Import các route từ controller
 from . import controller
--- app/blueprints/utils/helpers.py
+++ app/blueprints/utils/helpers.py
@@ -1,22 +1,30 @@
 from flask import render_template_string
 
+
 def render_template_util(env, template_name, **kwargs):
     template = env.get_template(template_name)
     return render_template_string(template.render(**kwargs))
 
+
 def create_blueprint(blueprint_name, import_name, template_folder="templates"):
     from flask import Blueprint
+
     return Blueprint(blueprint_name, import_name, template_folder=template_folder)
+
 
 def create_environment(file_path):
     from jinja2 import FileSystemLoader, Environment
     import os
 
     return Environment(
+        # Load template từ các thư mục khác nhau, dùng os.path.abspath(file_path) để tính đường dẫn
+        # từ vị trí của file_path truyền vào
         loader=FileSystemLoader(
             [
                 os.path.join(os.path.dirname(os.path.abspath(file_path)), "templates"),
-                os.path.join(os.path.dirname(os.path.abspath(file_path)), "..", "shared", "templates"),
+                os.path.join(
+                    os.path.dirname(os.path.abspath(file_path)), "..", "..", "layouts"
+                ),
             ]
         )
     )
